26af56d91a5c9c6665d4216e74e8e2794a403965
andylayzell <andy.layzell@gmail.com>
2015-01-10 07:18:39 +0000

    Script for creating a web app
    Useful script for creating a web application.

---
+++ CreateWebApplication.ps1
@@ -0,0 +1,4 @@
+﻿# New-SPSite -Url "http://fwosearch.edc.hosts.devnetwork" -OwnerAlias "FirstServis\Administrator" -Name "FWO Search" -Template "STS#1"
+
+$ap = New-SPAuthenticationProvider
+New-SPWebApplication -Name "FWO Search" -Port 80 -HostHeader fwosearch.edc.hosts.devnetwork -URL "http://fwosearch.edc.hosts.devnetwork" -ApplicationPool "fwosearch.edc.hosts.devnetwork" -ApplicationPoolAccount (Get-SPManagedAccount "FirstServis\Administrator") -AuthenticationProvider $ap
