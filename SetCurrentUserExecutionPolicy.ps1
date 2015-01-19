ebacae64f6bf55744ab886bf211a4b1f81ba4c33
andylayzell <andy.layzell@gmail.com>
2015-01-19 10:39:06 +0000

    Script for setting execution policy
    This script is used to set the correct permissions so that PowerShell
    scripts can be run from Visual Studio.

---
+++ SetCurrentUserExecutionPolicy.ps1
@@ -0,0 +1,2 @@
+﻿# This script is used to set the correct permissions so that PowerShell scripts can be run from Visual Studio.
+set-executionpolicy -scope CurrentUser -executionPolicy RemoteSigned -force
