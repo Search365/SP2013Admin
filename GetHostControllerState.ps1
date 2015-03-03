17c929cb2feca02a498a617b7c9e0aa2d43770cd
andylayzell <andy.layzell@gmail.com>
2015-03-03 23:25:04 +0000

    Gets status of Search Host Controller Service
    Gets status of Search Host Controller Service

---
+++ GetHostControllerState.ps1
@@ -0,0 +1,11 @@
+﻿<#
+$inst = Get-SPServiceInstance | ? {$_.TypeName -eq "Search Host Controller Service" } | ? { $_.PrimaryHostController -eq $true }
+$sh = Get-SPServiceInstance | ? {$_.SearchServiceInstanceId -eq $inst.SearchServiceInstanceId.ToString()}
+$sh.Status
+#>
+
+$hostname = "fsspe001"
+$sh = Get-SPServiceInstance | ? {$_.TypeName -eq “Search Host Controller Service”} | ? {$_.Server -match $hostname}
+$sh.Status
+
+Get-SPServiceInstance | sort TypeName | select TypeName, Status, Server, PrimaryHostController | ? {$_.TypeName -eq "Search Host Controller Service" }
