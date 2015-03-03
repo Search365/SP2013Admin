455bd4c85d66f93c8b317bbcbdd7f892273d052c
andylayzell <andy.layzell@gmail.com>
2015-03-03 23:19:18 +0000

    Updated with PowerShell for each step
    Added PowerShell to stop/start Timer service and update the cache.ini
    file.

--- fs15-search-hard-reset.ps1
+++ fs15-search-hard-reset.ps1
@@ -1,7 +1,15 @@
-# stop sptimerv4
-get-childitem "c:\ProgramData\Microsoft\SharePoint\Config" -include *.xml -recurse | foreach ($_) {remove-item $_.fullname} 
-# then replace contents of c:\ProgramData\Microsoft\SharePoint\Config\guid\cache.ini with the number "1"
+# Stop Timer Service.
+Get-service sptimerv4 | Stop-Service
+
+# Delete all xml files.
+$systemCache = "c:\ProgramData\Microsoft\SharePoint\Config"
+Get-ChildItem $systemCache -include *.xml -recurse | foreach ($_) {remove-item $_.fullname} 
+
+# Replace contents of c:\ProgramData\Microsoft\SharePoint\Config\guid\cache.ini with the number "1"
+Get-ChildItem $systemCache -include "cache.ini" -recurse | foreach ($_) { Set-Content $_.FullName "1" }
+
 # restart sptimerv4
+Get-service sptimerv4 | Start-Service
 
 # other options
 # $ssa.ForceResume($ssa.IsPaused())
