14539be0c27c8dd43513233499f7e7b68988a5b8
andylayzell <andy.layzell@gmail.com>
2015-03-03 23:25:55 +0000

    Reprovisions the Search Host Controller Service
    Use this script when the Search Host Controller Service is stuck.

---
+++ ReprovisionHostControllerState.ps1
@@ -0,0 +1,8 @@
+﻿$hostname = "fsspe001"
+$inst = Get-SPServiceInstance | ? {$_.TypeName -eq “Search Host Controller Service”} | ? {$_.Server -match $hostname}
+$sh = Get-SPServiceInstance | ? {$_.SearchServiceInstanceId -eq $inst.SearchServiceInstanceId.ToString()}
+$sh.Status
+$sh.Unprovision()
+$sh.Status
+$sh.Provision()
+$sh.Status
