09dc741774f81ee58df53f69f58f8e0abc34a0d9
andylayzell <andy.layzell@gmail.com>
2015-03-10 11:40:09 +1100

    Updated the path to match SharePoint
    Updated the path to Z:\sp2013\15\LOGS.
    Restarts the tracing service after updating the path.

--- CreateULSLogDirectory.ps1
+++ CreateULSLogDirectory.ps1
@@ -1,9 +1,12 @@
-﻿$destination = 'Z:\2013\15\LOGS'
+﻿$destination = 'Z:\sp2013\15\LOGS'
 
 # Check directory exists - if not create it.
 if(!(Test-Path -Path $destination )){
     New-Item -ItemType directory -Path $destination
     Write-Host "Directory Created: $destination"
+
+    Write-Host "Restarting SharePoint Tracing Service..."
+    Get-Service SPTraceV4 | Restart-Service
 }
 else
 {
