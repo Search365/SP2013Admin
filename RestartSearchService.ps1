b187c5b9b4d6ec032502a2ef5b5aa832955872d0
andylayzell <andy.layzell@gmail.com>
2015-03-10 11:53:43 +1100

    Updating the logging
    Using Write-Host for logging.

--- RestartSearchService.ps1
+++ RestartSearchService.ps1
@@ -1,6 +1,6 @@
 ﻿#####################################
 ### Restart the osearch15 service ###
 #####################################
-log "Restarting SharePoint 15 Search Service (osearch15)"
+Write-Host "Restarting SharePoint 15 Search Service (osearch15)"
 get-service osearch15 | restart-service
-log "SharePoint 15 Search Service (osearch15) successfully restarted"
+Write-Host "SharePoint 15 Search Service (osearch15) successfully restarted"
