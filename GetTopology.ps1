2cdd12d9735c66eba30dacc8806e97c3c33ada0b
andylayzell <andy.layzell@gmail.com>
2015-06-04 09:32:20 +1000

    New Topology scripts
    New scripts for getting the details of the active topology and removing a crawl component from the topology.

---
+++ GetTopology.ps1
@@ -0,0 +1,8 @@
+﻿$ssa = Get-SPEnterpriseSearchServiceApplication
+$active = Get-SPEnterpriseSearchTopology -SearchApplication $ssa -Active
+
+$activeComponents = Get-SPEnterpriseSearchComponent -SearchTopology $active
+$activeComponents
+
+$filename = Join-Path $PSScriptRoot "ActiveTopology.txt"
+$activeComponents > $filename
---
+++ RemoveCrawlComponentFromTopology.ps1
@@ -0,0 +1,14 @@
+﻿$ssa = Get-SPEnterpriseSearchServiceApplication
+$active = Get-SPEnterpriseSearchTopology -SearchApplication $ssa -Active
+$clone = New-SPEnterpriseSearchTopology -SearchApplication $ssa -Clone -SearchTopology $active
+
+Get-SPEnterpriseSearchComponent -SearchTopology $clone
+
+$crawlComponent = Get-SPEnterpriseSearchComponent -SearchTopology $clone | Where-Object { $_.Name -like "CrawlComponent*" }
+Remove-SPEnterpriseSearchComponent -Identity $crawlComponent -SearchTopology $clone
+
+Get-SPEnterpriseSearchComponent -SearchTopology $clone
+
+#Set-SPEnterpriseSearchTopology -Identity $clone
+
+Write-Host "Complete"
